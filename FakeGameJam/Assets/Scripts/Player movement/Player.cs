4cec7b243ca90c7c4bbcb49e388ec1b1eacf68e2
markdingli18 <103255544+markdingli18@users.noreply.github.com>
2022-12-03 08:18:26 +0100

    commitMornDay2

--- FakeGameJam/Assets/Scripts/Player movement/Player.cs
+++ FakeGameJam/Assets/Scripts/Player movement/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
@@ -16,44 +18,61 @@ public class Player : MonoBehaviour
   private bool isGrounded;
   private string GROUND_TAG = "Ground";
 
-  //Rigidbody rb;
+  //Press to start
+  private bool isStarted = false;
+  public Text startText;
+  private Rigidbody2D rb2d;
 
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    //rb = GetComponent<Rigidbody>();
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKey(KeyCode.D)) {
-        transform.Translate(Vector3.right * Time.deltaTime * player_movement_speed);
-    }
-
-    if (Input.GetKey(KeyCode.A)) {
-        transform.Translate(Vector3.left * Time.deltaTime * player_movement_speed);
-    }
-
-    if (jump_charge_remaining > 0 && isGrounded) {
-      if (Input.GetKeyDown(KeyCode.Space)) {
-        isGrounded = false;
-        transform.Translate(Vector3.up * player_jump_speed);
-        
-          
-      }
-      
-    }
-    
-    if (health == 0)
+    // Start is called before the first frame update
+    void Start()
     {
-      Destroy(this.gameObject);
+        Time.timeScale = 0f;
     }
-  }
 
-  private void OnCollisionEnter2D(Collision2D collision)
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Press to start
+        if (Input.GetMouseButtonDown(0) && isStarted == false)
+        {
+            Time.timeScale = 1f;
+            startText.gameObject.SetActive(false);
+
+        }
+        //-------------------------------------------------------------------------------------------------------------
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * player_movement_speed);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * player_movement_speed);
+            }
+
+            if (jump_charge_remaining > 0 && isGrounded)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    isGrounded = false;
+                    transform.Translate(Vector3.up * player_jump_speed);
+
+
+                }
+
+            }
+
+            if (health == 0)
+            {
+                Destroy(this.gameObject);
+            }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
 
       if (collision.gameObject.CompareTag(GROUND_TAG)) 
@@ -88,4 +107,3 @@ public class Player : MonoBehaviour
              health--;
          }
     }*/
-
